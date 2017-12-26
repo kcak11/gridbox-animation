f75f1d82b2bbe37a23bb0bd10866883d5e945868
K.C.Ashish Kumar <kcak11@gmail.com>
2017-12-26 12:41:47 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 (function() {
-	var _r = [],
-	_ctr = 0,
+	var _r = [], 
+	_ctr = 0, 
 	effects = [ "bg1", "bg1 bg2", "bg1 bg2 bg3", "bg1 bg2", "bg1", "" ], 
-	effectsCtr = 0,
-	tileElem,
-	gridBoxContainer = document.querySelector(".gridBoxContainer"),
-	preloadedImage = document.querySelector("#preloadedImage"),
+	effectsCtr = 0, 
+	tileElem, 
+	gridBoxContainer = document.querySelector(".gridBoxContainer"), 
+	preloadedImage = document.querySelector("#preloadedImage"), 
 	frag = document.createDocumentFragment();
 	for (var y = 0; y < 522; y += 29) {
 		for (var x = 0; x < 522; x += 29) {
@@ -32,10 +32,13 @@
 					e.classList.contains("visible") ? e.classList.remove("visible") : e.classList.add("visible");
 					if (idx === len - 1) {
 						setTimeout(function() {
-							play();
+							gridBoxContainer.classList.contains("animate") ? gridBoxContainer.classList.remove("animate") : gridBoxContainer.classList.add("animate");
 							document.querySelector("body").className = effects[effectsCtr % 6];
 							effectsCtr++;
-						}, 968);
+							setTimeout(function() {
+								play();
+							}, 1000);
+						}, 605);
 					}
 				}, i);
 			})(allTiles[_r[i]], allTiles.length, i, i * 10);
