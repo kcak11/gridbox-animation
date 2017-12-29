c2fcfd8511d27b79cc22a24a592e040549e1afb4
K.C.Ashish Kumar <kcak11@gmail.com>
2017-12-29 15:18:09 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,14 +1,8 @@
 (function() {
-	var _r = [],
-	_ctr = 0,
-	effects = [ "bg1", "bg1 bg2", "bg1 bg2 bg3", "bg1 bg2", "bg1", "" ],
-	effectsCtr = 0,
-	tileElem,
-	gridBoxContainer = document.querySelector(".gridBoxContainer"),
-	gridBoxSpinner = document.querySelector("._gboxS"),
-	preloadedImage = document.querySelector("#preloadedImage"),
-	frag = document.createDocumentFragment();
-	function createLayout(){
+	var _r = [], _ctr = 0, effects = [ "bg1", "bg1 bg2", "bg1 bg2 bg3", "bg1 bg2", "bg1", "" ], effectsCtr = 0, tileElem, gridBoxContainer = document.querySelector(".gridBoxContainer"), gridBoxSpinner = document.querySelector("._gboxS"), preloadedImage = document.querySelector("#preloadedImage"), frag = document.createDocumentFragment(), cFrag = document.createDocumentFragment(), animCtr = 0;
+
+	cFrag.appendChild(document.querySelector("#css_i"));
+	function createLayout() {
 		for (var y = 0; y < 522; y += 29) {
 			for (var x = 0; x < 522; x += 29) {
 				tileElem = document.createElement("div");
@@ -21,7 +15,7 @@
 				_ctr++;
 			}
 		}
-		gridBoxContainer.innerHTML="";
+		gridBoxContainer.innerHTML = "";
 		gridBoxContainer.appendChild(frag);
 	}
 
@@ -37,16 +31,36 @@
 					if (idx === len - 1) {
 						setTimeout(function() {
 							gridBoxContainer.classList.contains("animate") ? gridBoxContainer.classList.remove("animate") : gridBoxContainer.classList.add("animate");
-							document.querySelector("body").className = effects[effectsCtr % 6];
-							effectsCtr++;
+							if (animCtr > 0 && (animCtr % 4 === 2 || animCtr % 4 === 0)) {
+								document.querySelector("body").className = "inTransition";
+								setTimeout(function() {
+									if (animCtr % 4 === 2) {
+										applyCSSRule(cssTextI);
+										cFrag.querySelector("#css_i") && document.querySelector("head").appendChild(cFrag.querySelector("#css_i"));
+										document.querySelector("#css_default") && cFrag.appendChild(document.querySelector("#css_default"));
+										effectsCtr = 0;
+									} else if (animCtr % 4 === 0) {
+										applyCSSRule(cssText);
+										cFrag.querySelector("#css_default") && document.querySelector("head").appendChild(cFrag.querySelector("#css_default"));
+										document.querySelector("#css_i") && cFrag.appendChild(document.querySelector("#css_i"));
+										effectsCtr = 0;
+									}
+									document.querySelector("body").className = "";
+								}, 2000);
+							}
 							setTimeout(function() {
 								play();
-							}, 1000);
+							}, 2000);
 						}, 605);
+					}
+					if (idx % 46 === 45) {
+						document.querySelector("body").className = effects[effectsCtr % 6];
+						effectsCtr++;
 					}
 				}, i);
 			})(allTiles[_r[i]], allTiles.length, i, i * 22);
 		}
+		animCtr++;
 	}
 	var init = function() {
 		if (window.location.protocol.indexOf("http") === -1 || preloadedImage.complete) {
@@ -61,8 +75,8 @@
 		}
 	};
 	init();
-	document.oncontextmenu = function(e){
-		if(e && e.preventDefault){
+	document.oncontextmenu = function(e) {
+		if (e && e.preventDefault) {
 			e.preventDefault();
 		}
 		return false;
